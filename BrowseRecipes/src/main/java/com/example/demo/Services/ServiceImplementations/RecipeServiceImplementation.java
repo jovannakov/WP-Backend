f5a094c087a21e9c48b1c3e917e183cdf498bfe3
jovannakov <jovan.nakov@students.finki.ukim.mk>
2020-02-16 19:48:39 +0100

    created the repository for recipes

---
+++ BrowseRecipes/src/main/java/com/example/demo/Services/ServiceImplementations/RecipeServiceImplementation.java
@@ -0,0 +1,23 @@
+package com.example.demo.Services.ServiceImplementations;
+
+
+import com.example.demo.Models.Entities.Recipe;
+import com.example.demo.Repository.RecipesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecipeServiceImplementation {
+    private final RecipesRepository recipesRepository;
+
+
+    public RecipeServiceImplementation(RecipesRepository recipesRepository){
+        this.recipesRepository = recipesRepository;
+    }
+
+    public List<Recipe> ListAll(){
+        return this.recipesRepository.ListAll();
+    }
+
+}
---
+++ BrowseRecipes/src/main/java/com/example/demo/Services/ServiceInterfaces/RecipeService.java
@@ -0,0 +1,10 @@
+package com.example.demo.Services.ServiceInterfaces;
+
+import com.example.demo.Models.Entities.Recipe;
+
+import java.util.List;
+
+public interface RecipeService {
+
+    public List<Recipe> ListAll();
+}
