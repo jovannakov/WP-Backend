906037b7e6c00ef978f1e94c882926c67c4f4b27
jovannakov <jovan.nakov@students.finki.ukim.mk>
2020-02-16 19:21:01 +0100

    Created the repository for ingredients

---
+++ BrowseRecipes/src/main/java/com/example/demo/Repository/Jpa/JpaIngredient.java
@@ -0,0 +1,4 @@
+package com.example.demo.Repository.Jpa;
+
+public class JpaIngredient {
+}
---
+++ BrowseRecipes/src/main/java/com/example/demo/Repository/Jpa/JpaRepositoryIngredient.java
@@ -0,0 +1,4 @@
+package com.example.demo.Repository.Jpa;
+
+public interface JpaRepositoryIngredient {
+}
